9347abf716ab0ad7e1702f0c1f6ff020c14211e4
Gyan Prakash <gyangps1998@gmail.com>
2023-07-11 22:07:17 +0530

    some change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,38 +8,37 @@ function Home() {
   const [users, setUsers] = useState([]);
   const navigate = useNavigate();
   const userData = JSON.parse(localStorage.getItem("user"));
-//   useEffect(() => {
-//     if (userData) {
-//       console.log("home", userData);
-//     //   axios
-//     //     .post(endpoint + "user/update", {
-//     //       userEmail: userData.userEmail,
-//     //       value: true,
-//     //       name: "userStatus",
-//     //     })
-//     //     .then((res) => {
-//     //       console.log(res);
-//     //       // navigate('/')
-//     //     })
-//     //     .catch((err) => console.log("err"));
-//     }
-//     else{
-//         navigate('/signIn')
-//     }
-//     return () => {
-//       axios
-//         .post(endpoint + "user/update", {
-//           userEmail: userData.userEmail,
-//           value: false,
-//           name: "userStatus",
-//         })
-//         .then((res) => {
-//           console.log(res);
-//           // navigate('/')
-//         })
-//         .catch((err) => console.log("err"));
-//     };
-//   }, [userData]);
+  useEffect(() => {
+    if (userData) {
+      console.log("home", userData);
+      //   axios
+      //     .post(endpoint + "user/update", {
+      //       userEmail: userData.userEmail,
+      //       value: true,
+      //       name: "userStatus",
+      //     })
+      //     .then((res) => {
+      //       console.log(res);
+      //       // navigate('/')
+      //     })
+      //     .catch((err) => console.log("err"));
+    } else {
+      navigate("/signIn");
+    }
+    return () => {
+      axios
+        .post(endpoint + "user/update", {
+          userEmail: userData.userEmail,
+          value: false,
+          name: "userStatus",
+        })
+        .then((res) => {
+          console.log(res);
+          // navigate('/')
+        })
+        .catch((err) => console.log("err"));
+    };
+  }, [userData]);
   useEffect(() => {
     axios.get(endpoint + "activeUsers").then((res) => {
       console.log("users", res);
